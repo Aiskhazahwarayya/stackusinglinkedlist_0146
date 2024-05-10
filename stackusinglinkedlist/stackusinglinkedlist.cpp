8370ee94597bc234cfbc82ac398ce30c4d84d14a
Aiskhazahwarayya <aiskharayya307@gmail.com>
2024-05-10 10:58:32 +0700

    add prosedur pop

--- stackusinglinkedlist/stackusinglinkedlist.cpp
+++ stackusinglinkedlist/stackusinglinkedlist.cpp
@@ -29,4 +29,13 @@ public:
         cout << "Push value: " << value << endl;
         return value;
     }
+
+    void pop() {
+        if (isEmpty()) {
+            cout << "Stack is empty." << endl;
+        }
+        cout << "Popped value:" << top->data << endl;
+        top = top->next;
+    }
+
 };
